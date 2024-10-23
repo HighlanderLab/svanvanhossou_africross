954d6c8ed5d5ce9116980402c7ef90fb6161b6e0
SeyiV <155534272+SeyiV@users.noreply.github.com>
2024-10-23 11:37:37 +0200

    Update Cross_Synthetic_PWB.R

--- Cross_Synthetic_PWB.R
+++ Cross_Synthetic_PWB.R
@@ -17,7 +17,7 @@ names(HybridBulls_v) <- paste0("Village", c(1:nVillages))
 HybridRefPop_v <- vector("list", nVillages)
 names(HybridRefPop_v) <- paste0("Village", c(1:nVillages))
 
-# Create object to store populations at farm level
+# Create objects to store populations at farm level
 LocalCows_f <- Villages
 HybridCows_f <- Villages
 HybridOffsprings_f <- Villages
