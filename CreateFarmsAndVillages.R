8cb31759754d9bbb1d830d45a45c76a2a9cca48f
SeyiV <155534272+SeyiV@users.noreply.github.com>
2024-10-28 09:20:54 +0100

    Update CreateFarmsAndVillages.R

--- CreateFarmsAndVillages.R
+++ CreateFarmsAndVillages.R
@@ -23,33 +23,33 @@ CowOrder <- sample(Localcows@id, size = sum(cowSizes), replace = FALSE) # random
 
 #  Randomly assign cows to farms
 for (i in (1:length(Farms))) { # For each Farm
-  # Match randomised CowID with CowPopulation, Subset full cow population into Farm Pop & store Farm Pop
+  # Match randomised CowID with cow population, subset full cow population into Farm Pop & store Farm Pop
   Farms[[i]] <- Localcows[match(CowOrder[c(1:cowSizes[i])], Localcows@id)]
   CowOrder <- CowOrder[-c(1:cowSizes[i])] # Remove sampled CowIDs
 }
 
-# ---- Create Villages ----
+# ---- Create villages ----
 
 # Create list of villages
 Villages <- vector("list", nVillages)
 names(Villages) <- paste0("Village", c(1:nVillages))
 
-# Randomly assign farms to Villages
+# Randomly assign farms to villages
 FarmOrder <- sample( 1: nFarms, size = nFarms, replace = FALSE)
 start <- seq(1,nFarms, by=nFarms/nVillages)
 stop <- seq(nFarms/nVillages, nFarms, by=nFarms/nVillages)
 
-for (each in (1:nVillages)) { # For each Village
+for (each in (1:nVillages)) { # For each village
   # Villages[[each]] <- Farms[start[each]:stop[each]]
   Villages[[each]] <- Farms[FarmOrder[start[each]:stop[each]]]
 }
 
-# Assignment of Bulls to the Villages
+# Assignment of bulls to the villages
 # Exotic Breed
 ExoticBulls20_v <- AssignBull_v(ExoticBulls_Nucleus, nVillages, nBull_v)
 # Local Breed
 LocalBulls20_v <- AssignBull_v(LocalBulls_Nucleus, nVillages, nBull_v)
 
-# List of R objects before Crossbreeding (used to reset the R environment after each crossbreeding scenario)
+# List of R objects before crossbreeding (used to reset the R environment after each crossbreeding scenario)
 InitObjects <- ls()
 InitObjects <- c(InitObjects, "InitObjects")
